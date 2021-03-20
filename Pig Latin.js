886d44726803fd11084f79e3bd7a40519f0d0ef1
Brad Rokosz <brokosz@users.noreply.github.com>
2021-03-20 02:06:37 -0600

    Pig Latin

---
+++ Pig Latin.js
@@ -0,0 +1,12 @@
+// igPay atinLay
+
+function pre(str) {
+  const arr = str.split(' ')
+  return arr
+    .map((word) => {
+      return word.match(/[A-z]/i)
+        ? `${word.substr(1)}${word.substr(0, 1)}ay`
+        : word
+    })
+    .join(' ')
+}
