b42716b7ed428b27e375484e96258d442e12d0b7
Brad Rokosz <brokosz@users.noreply.github.com>
2021-03-20 10:21:31 -0600

    UUID No Dashes

---
+++ UUID No Dashes.js
@@ -0,0 +1,8 @@
+//Generates random UUID numbers without dashes
+
+function post(str) {
+    return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+      var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+      return v.toString(16);
+    });
+    }
